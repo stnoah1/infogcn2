b894f4832a5d57cd37828c71c20018cf5ada8662
sgchi <jisk984@snu.ac.kr>
2022-11-04 06:36:40 +0000

    concat fixed

--- model/infogcn.py
+++ model/infogcn.py
@@ -287,7 +287,7 @@ class InfoGCN(nn.Module):
             GCN(base_channel, base_channel, A),
             nn.Conv2d(base_channel, 3, 1),
         )
-        if self.z_pooling == None:
+        if self.z_pooling == "None":
             if n_step in [1,2,3]:
                 in_dim = base_channel*(n_step+1)
                 mid_dim = base_channel*(n_step+1)//2
